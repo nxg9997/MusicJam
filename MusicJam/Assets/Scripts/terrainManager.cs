3d1eba34ff22a84a8eecf4f4c33659b14001a7e7
nxg9997 <nxg9997@rit.edu>
2017-09-22 10:06:58 -0400

    shooting works now

---
+++ MusicJam/Assets/Scripts/terrainManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class terrainManager : MonoBehaviour {
+
+	public Vector3 terrainSize;
+	public int resolution;
+	private TerrainData tData;
+
+	// Use this for initialization
+	void Start () {
+		tData = gameObject.GetComponent<TerrainCollider> ().terrainData;
+		tData.size = terrainSize;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
