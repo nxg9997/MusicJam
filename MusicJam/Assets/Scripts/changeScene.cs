5492c9aa5922ce0d33790bf722bb897968eacbec
DavidXLiu <dxl4041@rit.edu>
2017-09-22 17:07:41 -0400

    Completed first level
    Credit to Nate for the level design and code for getting the first emitter. Added code for continuing to the next scene for the warp.

---
+++ MusicJam/Assets/Scripts/changeScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.name == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
