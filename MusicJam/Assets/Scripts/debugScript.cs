18efcfefa505e84dbcc6b7f49039d04b9f31cca2
DavidXLiu <dxl4041@rit.edu>
2017-09-22 11:42:10 -0400

    Changed Lighting
    Made the world pitch black. Added a debug script that will illuminate the area when 'L' is pressed

---
+++ MusicJam/Assets/Scripts/debugScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class debugScript : MonoBehaviour {
+
+    // Attributes
+    public Light light; // Place the light on the First Person Character into here
+    public Camera camera; // Place the First Person Camera into here
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Toggle "blind" on or off
+		if(Input.GetKeyDown(KeyCode.L))
+        {
+            if(camera.backgroundColor == Color.black)
+            {
+                camera.backgroundColor = Color.white;
+                light.enabled = true;
+            }
+            else
+            {
+                camera.backgroundColor = Color.black;
+                light.enabled = false;
+            }
+        }
+	}
+}
