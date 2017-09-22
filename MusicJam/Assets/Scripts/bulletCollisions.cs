50415bb46a9aa6344aba155153c9db0b30c948b4
nxg9997 <nxg9997@rit.edu>
2017-09-22 10:19:25 -0400

    bullet collisions generate sound clip, destroy bullet

--- MusicJam/Assets/Scripts/bulletCollisions.cs
+++ MusicJam/Assets/Scripts/bulletCollisions.cs
@@ -25,19 +25,8 @@ public class bulletCollisions : MonoBehaviour {
 	/// </summary>
 	/// <param name="collision">Collision.</param>
 	void OnCollisionEnter(Collision collision){
-		if (collision.gameObject.name.Contains ("Slasher")) { //checks if the bullet collides with a zombie
-			Destroy (collision.gameObject);
-			//counter.zCount++; //increases zombie kill count
-			GameObject bloodSpurt = Instantiate (blood, new Vector3(transform.position.x, transform.position.y, transform.position.z), transform.rotation); //creates instance of blood particle effect
-			Debug.Log ("HIT SLASHER!");
-		}
-		else if (collision.gameObject.name.Contains ("Broadleaf")) { //checks if the bullet hits a tree
-			Destroy (collision.gameObject);
-			//counter.tCount++; //increases tree destroy count
-			GameObject explo = Instantiate (explosion, new Vector3(transform.position.x, transform.position.y, transform.position.z), transform.rotation); //creates instance of the explosion particle effect
-			AudioSource.PlayClipAtPoint (explosionSound, transform.position); //plays explosion sound effect
-			Debug.Log ("HIT TREE!");
-		}
+		Debug.Log ("hit");
+		AudioSource.PlayClipAtPoint (explosionSound, transform.position);
 		Destroy (gameObject);
 
 	}
