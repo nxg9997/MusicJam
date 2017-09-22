9f42f357b585deb116b3d443af62c44348967be2
DavidXLiu <dxl4041@rit.edu>
2017-09-22 19:25:58 -0400

    Added levels and enemies
    Imported levels 1-5 created by Nate. Added in scripts to handle shooting enemies

---
+++ MusicJam/Assets/Scripts/bullet2Collisions.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet2Collisions : MonoBehaviour {
+
+	//attributes
+	//public countScript counter;
+	public GameObject explosion;
+	public AudioClip explosionSound;
+	public GameObject blood;
+
+    private GameObject player;
+    private ScannerEffect2Demo scanner2;
+
+	// Use this for initialization
+	void Start () {
+        //counter = GameObject.Find ("counter").GetComponent<countScript> ();
+        player = GameObject.Find("Player");
+        scanner2 = player.GetComponentInChildren<ScannerEffect2Demo>();
+        Debug.Log("Test");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	/// <summary>
+	/// Raises the collision enter event.
+	/// </summary>
+	/// <param name="collision">Collision.</param>
+	void OnCollisionEnter(Collision collision){
+		Debug.Log ("hit");
+		//AudioSource.PlayClipAtPoint (explosionSound, transform.position);
+
+        // Only triggers against enemies
+        if(collision.collider.gameObject.tag == "Enemy")
+        {
+            scanner2.isHit = true;
+        }
+        Destroy (gameObject);
+
+	}
+}
--- MusicJam/Assets/Scripts/bulletScript.cs
+++ MusicJam/Assets/Scripts/bulletScript.cs
@@ -6,10 +6,13 @@ public class bulletScript : MonoBehaviour {
 
 	//attributes
 	public GameObject bullet;
+    public GameObject bullet2;
 	public GameObject fpc;
 	public int bulletSpeed;
 	public AudioClip fired;
 	public bool EL1 = false;
+    public bool EL2 = false;
+    public bool ELA = false;
 
 	// Use this for initialization
 	void Start () {
@@ -35,5 +38,12 @@ public class bulletScript : MonoBehaviour {
 			//bullet.GetComponent<Rigidbody> ().velocity = new Vector3 (10, 0, 0);
 
 		}
+        if(Input.GetButtonDown("Fire2") && ELA)
+        {
+            GameObject projectile = Instantiate(bullet2, new Vector3(fpc.transform.position.x, fpc.transform.position.y, fpc.transform.position.z), fpc.transform.rotation) as GameObject;
+            projectile.transform.rotation = new Quaternion(fpc.transform.rotation.x, 90, fpc.transform.rotation.z, fpc.transform.rotation.w);
+
+            projectile.GetComponent<Rigidbody>().AddForce(fpc.transform.forward * bulletSpeed);
+        }
 	}
 }
