f5d1e44f3ceb718f4c5de373c6b29cbc8d27ab1e
nxg9997 <nxg9997@rit.edu>
2017-09-22 22:44:52 -0400

    up to 10:37

---
+++ MusicJam/Assets/Scripts/rogueEmitterScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rogueEmitterScript : MonoBehaviour {
+
+	public AudioSource song;
+	//public AudioClip song2;
+	public GameObject bullet;
+	private bool active = true;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+		//AudioSource.PlayClipAtPoint (song, transform.position);
+		//song.PlayOneShot(song.GetComponent<AudioClip>());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!active) {
+			//AudioSource.PlayClipAtPoint (song2, transform.position);
+			song.Stop();
+		}
+	}
+
+	void OnCollisionEnter(Collision collision){
+		if (collision.gameObject.name.Contains ("bullet2")) {
+			active = false;
+			gameObject.tag = "Untagged";
+		}
+	}
+}
--- MusicJam/Assets/Scripts/upgradeStation.cs
+++ MusicJam/Assets/Scripts/upgradeStation.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class upgradeStation : MonoBehaviour {
 
 	public GameObject shooter;
-	private bool complete = false;
+	public bool c1 = false;
+	public bool c2 = false;
+	public bool ca = false;
 
 	// Use this for initialization
 	void Start () {
@@ -18,9 +20,17 @@ public class upgradeStation : MonoBehaviour {
 	}
 
 	void OnMouseDown () {
-		if (!complete) {
-			shooter.GetComponent<bulletScript> ().EL1 = !shooter.GetComponent<bulletScript> ().EL1;
-			complete = true;
+		if (c1) {
+			shooter.GetComponent<bulletScript> ().EL1 = true;
+			c1 = true;
+		}
+		if (c2) {
+			shooter.GetComponent<bulletScript> ().EL2 = true;
+			c2 = true;
+		}
+		if (ca) {
+			shooter.GetComponent<bulletScript> ().ELA = true;
+			ca = true;
 		}
 	}
 }
